64add4264326287608f71c7ef9da57a48bc61a14
njmachin <nicholasjohnmachin@gmail.com>
2019-07-18 10:06:33 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ class App extends React.Component {
 
   state = {
     articles: articles,
+    view: "card",
+    light: true
+
   }
 
   renderArticles = () => {
+    
     return this.state.articles.map(article => {
+      if (this.state.view === "card") {
+      console.log(this.state)
       return <ArticleCard 
               key={article.id} 
               title={article.title}
@@ -21,15 +27,41 @@ class App extends React.Component {
               urlToImage={article.urlToImage}
               description={article.description}
               />
+      } else {
+        return <ArticleItem
+              key={article.id} 
+              title={article.title}
+              url={article.url}
+              urlToImage={article.urlToImage}
+              description={article.description}
+              />
+      }
     })
+  }
+  switchViews = () => {
+    if (this.state.view === 'card') {
+      this.setState({
+        view: "list"
+      }) 
+    } else {
+      this.setState({
+        view: "card"
+      })
+    }
+  }
+
+  changeView = () => {
+      this.setState({
+        light: !this.state.light
+      })
   }
 
   render(){
     return (
-      <div className="light">
-        <button>Switch to Dark Mode</button>
-        <button>Switch to List View</button>
-        <div className="cards">
+      <div className={this.state.light ? "light" : "dark"}>
+        <button onClick={this.changeView}>{this.state.light ? "Switch to Dark Mode" : "Switch to Light Mode"}</button>
+        <button onClick={this.switchViews}>{this.state.view === 'card' ? "Switch to List View" : "Switch to Card View"}</button>
+        <div className={this.state.view === 'card' ? "cards" : "list"}>
           {this.renderArticles()}
         </div>
       </div>
